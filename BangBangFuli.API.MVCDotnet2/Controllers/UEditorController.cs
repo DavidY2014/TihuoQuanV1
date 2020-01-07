832cd8c7ad1ca6307aeed6d121ee2f3c8cd84b6a
david <wu.xiguanghua@163.com>
2020-01-07 21:43:53 +0800

    init

---
+++ BangBangFuli.API.MVCDotnet2/Controllers/UEditorController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UEditorNetCore;
+
+namespace BangBangFuli.API.MVCDotnet2.Controllers
+{
+    [Route("api/[controller]")] //配置路由
+    public class UEditorController : Controller
+    {
+        private readonly UEditorService _ueditorService;
+        public UEditorController(UEditorService ueditorService)
+        {
+            _ueditorService = ueditorService;
+        }
+
+        public void Do()
+        {
+            _ueditorService.DoAction(HttpContext);
+        }
+
+    }
+}
